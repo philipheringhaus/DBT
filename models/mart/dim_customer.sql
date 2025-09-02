be518013f5cafbf986c1f888c637fa1b96d10db0
Philip Heringhaus <Philip.heringhaus@attollo.se>
2025-09-02 13:33:50 +0000

    Rebuild project

--- models/mart/dim_customer.sql
+++ models/mart/dim_customer.sql
@@ -1,13 +1,13 @@
 with customers as (
     select 
         *
-    from {{ ref('stg_customers') }}
+    from {{ ref('stg_customer') }}
 ),
 
 orders as (
     select
         *
-    from {{ ref('stg_orders') }}
+    from {{ ref('stg_order') }}
 ),
 
 customer_orders as(
